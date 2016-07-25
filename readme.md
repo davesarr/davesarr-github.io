c94a528032aab78f200f5e98f8556791c7130bcf
Daouda Sarr <davesarr@hotmail.com>
2016-07-25 10:11:50 -0400

    project1

---
+++ readme.md
@@ -0,0 +1,3 @@
+About my first project.
+
+Struggle:
---
+++ script.js
@@ -0,0 +1,77 @@
+
+  $(function(){
+  // array for name of cards
+    var myCards=["images/suicune.jpg","images/pika.jpg","images/psyduck","images/patrick.jpg","images/pika.jpg","images/charizard.jpg"];
+
+    var count= 0;
+//google reference
+    var mixingCards = function(){
+      var random = 0;
+      for(var i = 0, i< mycards.length,i++){
+        randomCards= Math.floor(Math.random()*mycards.length)
+      };
+      }
+      mixingCards();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $('#play').click(function() {
+    $('#play').remove();
+  });
+
+  //  $('#restart').click(function() {
+  //   $('#we').fadeIn(200)
+  // });
+  //   var body= $("body");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
