d07150df6ae80188c7da256f9d75e50cf733af24
Daouda Sarr <davesarr@hotmail.com>
2016-07-26 09:15:15 -0400

    my files

--- script.js
+++ script.js
@@ -1,75 +1,90 @@
 
-  $(function(){
+$(function(){
+    console.log("ready");
   // array for name of cards
-    var myCards=["images/suicune.jpg","images/pika.jpg","images/psyduck","images/patrick.jpg","images/pika.jpg","images/charizard.jpg"];
+    var myCards=[
+      "images/suicune.jpg",
+      "images/pika.jpg",
+      "images/psyduck.jpg",
+      "images/patrick.jpg",
+      "images/pika.jpg",
+      "images/charizard.jpg"
+  ];
 
-    var count= 0;
-//google reference
-    var mixingCards = function(){
-      var random = 0;
-      for(var i = 0, i< mycards.length,i++){
-        randomCards= Math.floor(Math.random()*mycards.length)
-      };
-      }
-      mixingCards();
+    var card1 = myCards[0];
+    var card2 = myCards[1];
+    var card3 = myCards[2];
+    var card4 = myCards[3];
+    var card5 = myCards[4];
+    var card6 = myCards[5];
 
-
-
-
-
-
-
-
-
+    var pika = 0;
+    var moves= 0;
 
 
+var addCardClickListener = function(){
 
+  $('.cardBack').click(function(e) {
+    // Generate a random number
+        var randomNumber = Math.round(Math.random()*(myCards.length-1));
 
+    // Pop random image out of myCards and store it in a new variable
+    var image= myCards.splice(randomNumber, 1);
 
-
-
-
-
-
-
-
-
-
-  $('#play').click(function() {
-    $('#play').remove();
+    // Change e.target css background image to random image
+    $(e.target).css('background-image','url("'+image+'")');
+    if(image == "images/pika.jpg"){
+      pika++;
+    } else{
+      moves++;
+    }
+    whoWins();
+    $(e.target).off('click');
   });
+
+};
+var whoWins = function(){
+  if(pika>1){
+    alert("you a winner");
+  }
+}
+
+
+
+addCardClickListener();
+  //     });
+
+  // })
 
   //  $('#restart').click(function() {
   //   $('#we').fadeIn(200)
   // });
   //   var body= $("body");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
